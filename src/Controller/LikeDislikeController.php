f778a42d4d56c024b21384a05ebc182a03b1d940
Herschell <juli1bellot@gmail.com>
2025-04-21 19:39:38 +0200

    base logic of likes/dislikes

---
+++ src/Controller/LikeDislikeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Like;
+use App\Entity\Post;
+use App\Entity\User;
+use App\Entity\Dislike;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class LikeDislikeController extends AbstractController
+{
+    #[Route('/add-like-dislike', name: 'add_like_dislike')]
+    public function index(EntityManagerInterface $entity_manager, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if($data['action'] == 'like'){
+            $like = new Like();
+
+            $like
+                ->setPost($entity_manager->getRepository(Post::class)->findOneBy(['id' => $data['postId']]))
+                ->setUser($entity_manager->getRepository(User::class)->findOneBy(['id' => $data['userId']]));
+
+            $entity_manager->persist($like);
+
+            $entity_manager->flush();
+        }
+        else{
+            $dislike = new Dislike();
+
+            $dislike
+                ->setPost($entity_manager->getRepository(Post::class)->findOneBy(['id' => $data['postId']]))
+                ->setUser($entity_manager->getRepository(User::class)->findOneBy(['id' => $data['userId']]));
+
+            $entity_manager->persist($dislike);
+
+            $entity_manager->flush();
+        }
+
+        return new JsonResponse(['message' => 'Action enregistrée !']);
+    }
+}
